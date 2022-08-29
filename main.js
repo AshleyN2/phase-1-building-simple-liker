79e1a4dbe79c1b13f85cf45bebe0fa6026d5076b
Ashley <ashleynjeri7@gmail.com>
2022-08-29 04:14:47 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartsData = document.querySelectorAll(".like-glyph");
 
+function likeKey(e) {
+  const heart = e.target;
+  mimicServerCall("http://mimicServer.example.com")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "full-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
+for (const item of heartsData) {
+  item.addEventListener("click", likeKey);
+}
 
 
 //------------------------------------------------------------------------------
